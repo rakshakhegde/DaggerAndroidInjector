f533ddc5a9156548efceb6deab48948123ad1de1
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-05-04 13:29:02 +0530

    MainModule update

--- app/src/main/java/me/rakshakhegde/dagger_android_injector/screens/main_screen/MainModule.java
+++ app/src/main/java/me/rakshakhegde/dagger_android_injector/screens/main_screen/MainModule.java
@@ -2,6 +2,7 @@ package me.rakshakhegde.dagger_android_injector.screens.main_screen;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by rakshakhegde on 26/04/17.
@@ -11,4 +12,11 @@ public abstract class MainModule {
 
 	@Binds
 	abstract MainView mainView(MainActivity act);
+
+	@Provides
+	static String providesUnusedData() {
+		return "Unused Data";
+	}
+
+	// Add more @Provides here
 }
