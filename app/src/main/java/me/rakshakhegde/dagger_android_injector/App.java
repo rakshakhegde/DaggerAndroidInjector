77b0d2c9c7b13bbbdaced593a43a71ecf9bf2255
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-05-04 14:40:13 +0530

    cleanup

--- app/src/main/java/me/rakshakhegde/dagger_android_injector/App.java
+++ app/src/main/java/me/rakshakhegde/dagger_android_injector/App.java
@@ -3,8 +3,6 @@ package me.rakshakhegde.dagger_android_injector;
 import dagger.android.DaggerApplication;
 import me.rakshakhegde.dagger_android_injector.dependencies.AppComponent;
 import me.rakshakhegde.dagger_android_injector.dependencies.DaggerAppComponent;
-//import me.rakshakhegde.dagger_android_injector.dependencies.DaggerAppComponent;
-
 
 /**
  * Created by rakshakhegde on 26/04/17.
