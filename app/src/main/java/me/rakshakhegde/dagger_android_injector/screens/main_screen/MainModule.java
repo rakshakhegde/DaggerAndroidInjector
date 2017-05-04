d22f214ba8c72be90e8d86c875999287e39a5daf
Rakshak R.Hegde <rakshakhegde@gmail.com>
2017-05-04 13:27:31 +0530

    @Binds

--- app/src/main/java/me/rakshakhegde/dagger_android_injector/screens/main_screen/MainModule.java
+++ app/src/main/java/me/rakshakhegde/dagger_android_injector/screens/main_screen/MainModule.java
@@ -1,16 +1,14 @@
 package me.rakshakhegde.dagger_android_injector.screens.main_screen;
 
+import dagger.Binds;
 import dagger.Module;
-import dagger.Provides;
 
 /**
  * Created by rakshakhegde on 26/04/17.
  */
 @Module
-public class MainModule {
+public abstract class MainModule {
 
-	@Provides
-	MainView mainView(MainActivity act) {
-		return act;
-	}
+	@Binds
+	abstract MainView mainView(MainActivity act);
 }
